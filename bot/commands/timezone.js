1a096f1ec4e76bd205ba8952983b064fd93fe4f2
C.E. Gagne <ceg9498@gmail.com>
2020-01-17 15:42:52 -0500

    Move zones definition to a separate file.

--- bot/commands/timezone.js
+++ bot/commands/timezone.js
@@ -1,27 +1,5 @@
 const moment = require('moment-timezone');
-
-const zones = new Map([
-  ['ast', 'Canada/Atlantic'],
-  ['adt', 'Canada/Atlantic'],
-  ['est', 'US/Eastern'],
-  ['edt', 'US/Eastern'],
-  ['cst', 'US/Central'],
-  ['cdt', 'US/Central'],
-  ['mst', 'US/Mountain'],
-  ['mdt', 'US/Mountain'],
-  ['arizona', 'US/Arizona'],
-  ['pst', 'US/Pacific'],
-  ['pdt', 'US/Pacific'],
-  ['akst', 'US/Alaska'],
-  ['akdt', 'US/Alaska'],
-  ['alaska', 'US/Alaska'],
-  ['hst', 'US/Hawaii'],
-  ['hdt', 'US/Hawaii'],
-  ['hawaii', 'US/Hawaii'],
-  ['sanger', 'America/Guayaquil'],
-  ['ect', 'America/Guayaquil'],
-  ['jst', 'Japan']
-]);
+const zones = require('../zones');
 
 function convertTime(args){
   let convertObj = formatInput(args);
---
+++ bot/zones.js
@@ -0,0 +1,24 @@
+module.exports = {
+  zones: new Map([
+    ['ast', 'Canada/Atlantic'],
+    ['adt', 'Canada/Atlantic'],
+    ['est', 'US/Eastern'],
+    ['edt', 'US/Eastern'],
+    ['cst', 'US/Central'],
+    ['cdt', 'US/Central'],
+    ['mst', 'US/Mountain'],
+    ['mdt', 'US/Mountain'],
+    ['arizona', 'US/Arizona'],
+    ['pst', 'US/Pacific'],
+    ['pdt', 'US/Pacific'],
+    ['akst', 'US/Alaska'],
+    ['akdt', 'US/Alaska'],
+    ['alaska', 'US/Alaska'],
+    ['hst', 'US/Hawaii'],
+    ['hdt', 'US/Hawaii'],
+    ['hawaii', 'US/Hawaii'],
+    ['sanger', 'America/Guayaquil'],
+    ['ect', 'America/Guayaquil'],
+    ['jst', 'Japan']
+  ])
+}
