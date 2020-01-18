cf155554af8209b85c9ec03130523545ebaf7839
Emily Gagne <ceg9498@gmail.com>
2020-01-18 15:58:53 -0500

    Updated reload to allow loading new commands.

--- bot/commands/reload.js
+++ bot/commands/reload.js
@@ -9,16 +9,22 @@ module.exports = {
     const command = message.client.commands.get(commandName) 
       || message.client.commands.find(c => c.aliases && c.aliases.includes(commandName));
     if(!command){
-      return message.channel.send(`${commandName} does not exist`);
+      message.channel.send(`${commandName} does not exist; will attempt to load`);
+      loadFile(message, commandName, "load");
+      return;
     }
-    delete require.cache[require.resolve(`./${commandName}.js`)];
-    try {
-      const newCommand = require(`./${commandName}.js`);
-      message.client.commands.set(newCommand.name, newCommand);
-      message.channel.send(`Successfully reloaded command: ${commandName}`);
-    } catch(err) {
-      console.error(err);
-      message.channel.send(`There was an error when attempting to reload ${commandName}`);
-    }
+    delete require.cache[require.resolve(`./${command.name}.js`)];
+    loadFile(message, command.name, "reload");
+  }
+}
+
+function loadFile(message, cName, loadType){
+  try {
+    const newCommand = require(`./${cName}.js`);
+    message.client.commands.set(newCommand.name, newCommand);
+    message.channel.send(`Successfully ${loadType}ed command: ${newCommand.name}`);
+  } catch(err) {
+    console.error(err);
+    message.channel.send(`There was an error when attempting to ${loadType} ${cName}`);
   }
 }
